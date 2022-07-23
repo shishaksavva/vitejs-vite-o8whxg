65deb8d1f5639aab71a2d726585d16b33c4528d2
shishaksavva <schishak.s@yandex.ru>
2022-07-23 14:32:11 +0300

    3 files modified, 2 added

---
+++ src/components/repoInfo/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div
+    class="
+      p-2
+      max-w-[100%]
+      w-[400px]
+      border border-black
+      rounded
+      text-left
+      bg-white
+      text-black
+      dark:border-white dark:bg-black dark:text-white
+    "
+  >
+    <div class="text-2xl text-center">
+      {{ repo.name }}
+    </div>
+    <div class="mt-5">
+      {{ repo.description }}
+    </div>
+    <div class="mt-5">
+      Страница:
+      <a
+        :href="repo.html_url"
+        target="_blank"
+        class="text-indigo-500 underline hover:text-indigo-700 cursour-pointer"
+        >{{ repo.html_url }}</a
+      >
+    </div>
+    <div class="mt-5 px-3 flex justify-between w-[100%] items-center">
+      <div class="text-center">
+        <div class="text-lg">{{ repo.stargazers_count }}</div>
+        <div class="text-md">Звезд</div>
+      </div>
+      <div class="text-center">
+        <div class="text-lg">{{ repo.language }}</div>
+      </div>
+      <div class="flex flex-col items-center">
+        <img
+          :src="repo.owner.avatar_url"
+          alt=""
+          class="
+            h-[25px]
+            w-[25px]
+            border border-black
+            rounded-[50%]
+            mr-1
+            object-cover object-center
+          "
+        />
+        <div class="text-md">{{ repo.owner.login }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    repo: Object,
+  },
+  data() {
+    return {
+      copied: false,
+    };
+  },
+  methods: {
+    copyGit() {
+      navigator.clipboard.writeText(repo.git_url);
+    },
+  },
+};
+</script>
--- src/components/searchForm/index.vue
+++ src/components/searchForm/index.vue
@@ -1,56 +1,68 @@
 <template>
-  <div :class="'container mx-auto p-2 max-w-lg' + darkClass">
-    <div
-      class="fixed cursor-pointer hover:font-bold top-[10px] right-[10px]"
-      @click="darkTheme = !darkTheme"
-    >
-      {{ darkTheme ? 'Светлая тема' : 'Темная тема' }}
-    </div>
-    <div
-      class="
-        fixed
-        top-[0]
-        left-[0]
-        w-[100vw]
-        h-[100vh]
-        bg-[#fff]
-        dark:bg-[#000]
-        z-[-100]
-      "
-    ></div>
-    <form @submit.prevent="search">
-      <div class="relative">
-        <search-icon />
-        <app-input
-          placeholder="Введите название репозитория..."
-          v-model="query"
-        />
-        <btn v-if="!!query" type="submit">Поиск</btn>
+  <div
+    :class="
+      'container mx-auto justify-center flex flex-row overflow-hidden h-[calc(100% - 60px)]' +
+      darkClass
+    "
+  >
+    <repo-info
+      v-if="selectedRepo"
+      :repo="selectedRepo"
+      @close="selectedRepo = null"
+    ></repo-info>
+    <div class="max-w-3xl grid grid-list w-[100%] overflow-hidden p-2">
+      <bg :mode="darkTheme" @toggle="darkTheme = !darkTheme">
+        {{ darkTheme ? 'Светлая тема' : 'Темная тема' }}</bg
+      >
+      <form @submit.prevent="search">
+        <div class="relative">
+          <search-icon />
+          <app-input
+            placeholder="Введите название репозитория..."
+            v-model="query"
+          />
+          <btn v-if="!!query" type="submit">Поиск</btn>
+        </div>
+      </form>
+      <div class="mt-10 h-[100%] border-box overflow-auto">
+        <list-item
+          v-for="repo of repos"
+          :key="repo.id"
+          @click="selectedRepo = repo"
+        >
+          <div class="flex justify-between items-center">
+            <span
+              class="
+                flex flex-row
+                alignitems-end
+                text-black
+                dark:text-white
+                text-2xl
+              "
+            >
+              {{ repo.name }}
+              <chip v-if="repo.language" color="green">{{
+                repo.language
+              }}</chip>
+            </span>
+            <chip color="blue">
+              <chip-img :src="repo.owner.avatar_url" />
+              {{ repo.owner.login }}
+            </chip>
+          </div>
+          <div class="text-grey text-sm">
+            {{ repo.description }}
+          </div>
+          <star> {{ repo.stargazers_count }} звезд </star>
+        </list-item>
       </div>
-    </form>
-    <div class="mt-10">
-      <list-item v-for="repo of repos" :key="repo.id">
-        <div class="flex justify-between items-center">
-          <span class="text-black dark:text-white text-2xl">{{
-            repo.name
-          }}</span>
-          <chip>
-            <chip-img :src="repo.owner.avatar_url" />
-            {{ repo.owner.login }}
-          </chip>
-        </div>
-        <div class="text-grey text-sm">
-          {{ repo.description }}
-        </div>
-        <star> {{ repo.stargazers_count }} звезд </star>
-      </list-item>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref, computed } from 'vue';
-import { useSearch } from './useSearch';
+import { useSearch, Repo } from './useSearch';
 import Star from '../ui/star.vue';
 import Btn from '../ui/btn.vue';
 import AppInput from '../ui/input.vue';
@@ -58,9 +70,12 @@ import SearchIcon from '../ui/search-icon.vue';
 import ListItem from '../ui/list-item.vue';
 import Chip from '../ui/chip.vue';
 import ChipImg from '../ui/chip-img.vue';
+import Bg from '../ui/bg.vue';
+import RepoInfo from '../repoInfo/index.vue';
 
 const { query, repos, search } = useSearch();
 const darkTheme = ref(false);
+const selectedRepo = ref<Repo | null>(null);
 
 const darkClass = computed({
   get() {
@@ -68,3 +83,21 @@ const darkClass = computed({
   },
 });
 </script>
+
+<style>
+.container {
+  height: calc(100vh - 60px);
+  box-sizing: border-box;
+}
+.grid-list {
+  grid-template-rows: max-content 1fr;
+}
+@media screen and (max-width: 800px) {
+  .container {
+    flex-direction: column !important;
+    justify-content: flex-start !important;
+    overflow: auto;
+    height: auto;
+  }
+}
+</style>
